b989411e75881f7ff295b5293ec5e520684eff16
Dwi Wahyu Prambodo <71806064+DwiWahyuP@users.noreply.github.com>
2021-03-24 21:45:48 +0700

    Minggu 3 Tugas 2

---
+++ Latihan_laravel/app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use illuminate\Http\Request;
+
+class ManagementUserController extends Controller  
+{
+    public function index (){
+        return "Halo ini adalah method index, dalam Controller ManagementUser";
+    }
+
+    public function create (){
+        return "Method ini nantinya akan digunakan untuk menampilkan form untuk menambah data user";
+    }
+
+    public function socket (Request $Request){
+        return "Method ini nantinya akan digunakan untuk manciptakn data user baru";
+    }
+
+    public function show ($id){
+        return "Method ini nantinya akan digunakan untuk mengambil satu data user dengan id =" . $id;
+    }
+
+    public function edit ($id){
+        return "Method ini nantinya akan digunakan untuk menampilkan form untuk mengubah data edit dengan id =" . $id;
+    }
+
+    public function update (Request $Request, $id){
+        return "Method ini nantinya akan digunakan untuk mengubah data user dengan id =" . $id;
+    }
+
+    public function destroy ($id){
+        return "Method ini nantinya akan digunakan untuk menghapus data user dengan id =" . $id;
+    }
+}
+
